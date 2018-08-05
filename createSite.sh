5b933a2e397b049f05a37bfb6b9e134734ca40e0
Pavel <pavel.bykov2018@mail.ru>
2018-08-05 13:00:36 +0300

    fix bugs, and complete script createSite.sh

--- createSite.sh
+++ createSite.sh
@@ -9,10 +9,10 @@ F_NORMAL='\033[0m'
 PATH_NGINX=("/etc/nginx/sites-available" "/etc/nginx/sites-enabled")
 PATH_APACHE=("/etc/apache2/sites-available" "/etc/apache2/sites-enabled")
 PATH_DIRECTORY="null" # Путь до директории где распологается сайт
+SELECT_SITE="null" # Выбранный сайт в методе editSite
 
 SELECT_SERVER="null"
 SELECT_USER="null"
-
 NAME_SITE="null"
 
 tput sgr0 
@@ -22,10 +22,6 @@ function checkUser {
     [ -d /home/$1 ] 
 }
 
-function getIp {
-    ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'
-}
-
 # Метод создающий конфигурационный файл apache || nginx
 function createConfig {
     if [[ $SELECT_SERVER == "apache" ]]
@@ -33,17 +29,23 @@ function createConfig {
     
         export CONFIGAPACHE=$(cat <<END
 <VirtualHost *:80>
-    ServerName $1
+    ServerName $NAME_SITE
     DocumentRoot $PATH_DIRECTORY
 
-    ErrorLog /home/$SELECT_USER/logs/$1/error.log
-    CustomLog /home/$SELECT_USER/logs/$1/error.log combined
+    <Directory $PATH_DIRECTORY>
+        Options -Indexes +FollowSymLinks +MultiViews
+        AllowOverride All
+        Require all granted
+    </Directory>
+
+    ErrorLog /home/$SELECT_USER/logs/$NAME_SITE/error.log
+    CustomLog /home/$SELECT_USER/logs/$NAME_SITE/error.log combined
 </VirtualHost>
 END
     );
     
-        echo "$CONFIGAPACHE" >> "${PATH_APACHE[0]}/$1.conf"
-        ln -s "${PATH_APACHE[0]}/$1.conf" "${PATH_APACHE[1]}/$1.conf"
+        sudo echo "$CONFIGAPACHE" >> "${PATH_APACHE[0]}/$NAME_SITE.conf"
+        sudo a2ensite "$NAME_SITE.conf"
     
     elif [[ $SELECT_SERVER == "nginx" ]]
     then
@@ -53,38 +55,36 @@ END
     fi
 }
 
-# Метод добовляющий в файл hosts доменное имя
-function appendDomenToHostsFile {
-    echo -en "\n${UNDERLINE}Укажите доменное имя:${F_NORMAL}"
-    read domenSite
-
-    echo -en "\n${UNDERLINE}Доменное имя: $domenSite, подтвердить?(y|n)${F_NORMAL}"
-    read selectAccept
-    if [[ $selectAccept != "n" || $selectAccept != "т" ]]
-    then
-        echo $(getIp)  $domenSite >> /etc/hosts
-        echo -en "\n${UNDERLINE}Сайт успешно создан.\nДиректория сайта /home/$SELECT_USER/$NAME_SITE\nДиректория логов /home/$SELECT_USER/logs/$NAME_SITE\n ${F_NORMLA}"
-    fi
-}
-
 function createSite {
     echo -en "\n${UNDERLINE}Укажите название сайта:${F_NORMAL}"
     read nameSites
+    NAME_SITE=$nameSites
+
+    # Подтверждение действия
+    echo -en "\n${UNDERLINE}Название сайта $NAME_SITE, подтвердить?(y|n)${F_NORMAL}"
+    read selectAccept
+    if [[ $selectAccept == "n" ]]
+    then
+        createSite
+    fi
     
     echo -en "\n${UNDERLINE}Создание директории сайта...${F_NORMAL}"
-    PATH_DIRECTORY="/home/$SELECT_USER/sites/$nameSites"
-    mkdir /home/$SELECT_USER/sites/$nameSites
-    mkdir /home/$SELECT_USER/logs/$nameSites
+    PATH_DIRECTORY="/home/$SELECT_USER/sites/$NAME_SITE"
+    mkdir /home/$SELECT_USER/sites/$NAME_SITE
+    mkdir /home/$SELECT_USER/logs/$NAME_SITE
+    sudo echo "<html><head><title>Test page</title></head><body>Page for check running site</body></html>" >> "/home/$SELECT_USER/sites/$NAME_SITE/index.html"
+    sudo chmod -R 777 /home/$SELECT_USER/sites/$NAME_SITE
+    sudo chmod -R 777 /home/$SELECT_USER/logs/$NAME_SITE
     echo -en "${GREEN}[OK]${NORMAL}\n"
 
     echo -en "${UNDERLINE}Создание конфигурационного файла...${F_NORMAL}"
-    createConfig $nameSites
-    NAME_SITE=$nameSites
+    createConfig
     echo -en "${GREEN}[OK]${NORMAL}\n"
 
+    # Перезагрузка сервиса
     if [[ $SELECT_SERVER == "apache" ]]
     then
-        echo -en "${UNDERLINE}Перезагрузка сервисов...${F_NORMAL}"
+        echo -en "${UNDERLINE}Перезагрузка сервиса...${F_NORMAL}"
         service apache2 restart
         echo -en "${GREEN}[OK]${NORMAL}\n"
     elif [[ $SELECT_SERVER == "nginx" ]]
@@ -92,17 +92,85 @@ function createSite {
         echo -en "${RED}Поддержка создания конфигурационного файла для nginx в разработке${NORMAL}"
         # Добавить откат действий
         exit
-    fi
-    appendDomenToHostsFile
+    fi 
+    echo -en "\n${UNDERLINE}Сайт успешно создан.\nДиректория сайта /home/$SELECT_USER/$NAME_SITE\nДиректория логов /home/$SELECT_USER/logs/$NAME_SITE\n ${F_NORMAL}"
+}
 
+# Удаление конфигураций
+function removeConfiguration {
+    # Проверка существования конфигурации
+    if ! [ -f /etc/apache2/sites-available/$SELECT_SITE.conf ]
+    then
+        echo -en "\n${UNDERLINE}Файл конфигурации отсутствует...${F_NORMAL}"
+    else
+        sudo a2dissite $SELECT_SITE.conf
+        sudo rm /etc/apache2/sites-available/$SELECT_SITE.conf
+    fi
+}
+
+# Удаление директории
+function removeDirectory {
+    sudo rm -r /home/$SELECT_USER/sites/$SELECT_SITE
+    sudo rm -r /home/$SELECT_USER/logs/$SELECT_SITE
 }
 
 function removeSite {
-    echo -en ""   
+    removeConfiguration # Удаляем конфигурации
+    removeDirectory # Удаляем директории
+    sudo systemctl reload apache2
 }
 
 function editSite {
-    echo -en ""
+    if [[ $SELECT_SERVER == "nginx" ]]
+    then
+        echo -en "\n${UNDERLINE}${RED}Подддержка nginx пока не доступна в изменение сайта${NORMAL}${F_NORMAL}"
+        exit
+    fi
+
+    echo -en "\n${UNDERLINE}Список ранее созданных сайтов:\n${F_NORMAL}"
+    
+    TEMP_COUNTER=1
+    arrayfiles=()
+
+    # Выводим список сайтов (директорий)
+    for file in `find /home/$SELECT_USER/sites -maxdepth 1 | grep /home/$SELECT_USER/sites/[a-zA-Z0-9]`
+    do
+        echo -en "$TEMP_COUNTER. "
+        TEMP_TEXT=`echo $file | sed 's/\/home\/[a-z]*\/sites\///g'`
+        echo $TEMP_TEXT
+        TEMP_COUNTER=$[TEMP_COUNTER +1]
+        arrayfiles[${#arrayfiles[*]}]=$TEMP_TEXT
+    done
+    read selectCounter
+    selectCounter=$[selectCounter -1]
+
+    # Проверка существования директории
+    if ! [ -d /home/$SELECT_USER/sites/${arrayfiles[$selectCounter]} ];
+    then
+        echo -en "${RED}ВНИМАНИЕ ДАННОЙ ДИРЕКТОРИИ НЕ СУЩЕСТВУЕТ${NORMAL}"
+        editSite
+    fi
+    
+    # Подтверждение выбора
+    echo -en "\n${UNDERLINE}Выбран ${arrayfiles[$selectCounter]}, подтвердить?(y|n)${F_NORMAL}"
+    read selectAccept
+
+    if [[ $selectAccept == "n" ]]
+    then
+        editSite;
+    fi
+
+    # Запись в глобальную переменную
+    SELECT_SITE=${arrayfiles[$selectCounter]}
+
+    # Выберем действие уже с ранее выбранным сайтом
+    echo -en "\n${UNDERLINE}Выберите действие:\n1.Удалить сайт и конфигурационный файл\n${F_NORMAL}"
+    read selectAction
+
+    case $selectAction in 
+        1)
+            removeSite;;
+    esac
 }
 
 function requestSelectUser {
@@ -115,18 +183,6 @@ function requestSelectUser {
         echo -en "${UNDERLINE}Данного пользователя не существует${F_NORMAL}\n"
         requestSelectUser;
     fi
-}
-
-function requestDoIt {
-    echo -en "\n${UNDERLINE}Выберите что нужно сделать:\n1.Создать сайт\n2.Изменить сайт${F_NORMAL}\n"
-    read doChoice
-
-    case $doChoice in
-        1)
-            createSite;;
-        2)
-            editSite;;
-    esac
 }
 
 # Метод выбирающий на каком пакете создать сайт (nginx || apache)
@@ -151,6 +207,17 @@ function requestSelectServer {
     fi
 }
 
+function requestDoIt {
+    echo -en "\n${UNDERLINE}Выберите что нужно сделать:\n1.Создать сайт\n2.Изменить сайт${F_NORMAL}\n"
+    read doChoice
+
+    case $doChoice in
+        1)
+            createSite;;
+        2)
+            editSite;;
+    esac
+}
 
 function main {
     requestSelectUser # Выбор пользователя
